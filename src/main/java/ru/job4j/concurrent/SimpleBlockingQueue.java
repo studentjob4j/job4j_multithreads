a9baa129654c88cb4ceb42becfb052e13520e742
evgenshegai <evgeniishegai@yandex.ru>
2021-05-28 17:48:54 +1000

    Реализовать шаблон Producer Consumer

---
+++ src/main/java/ru/job4j/concurrent/SimpleBlockingQueue.java
@@ -0,0 +1,63 @@
+package ru.job4j.concurrent;
+
+/**
+ * @author Shegai Evgenii
+ * @version 1.0
+ * @since 27.05.2021
+ * Данный класс реализует блокирующую очередь на основе линкедлиста. Если очередь
+ * пустая , то Producer  добавляем значение до тех пор пока размер не станет равным установленному
+ * пределу .Далее очередь засыпает .  Consumer берет из очереди значения и если очередь пустая ,
+ * нить засыпает .
+ */
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+    private final int size;
+
+    public SimpleBlockingQueue(int size) {
+        this.size = size;
+    }
+
+    public void offer(T value) {
+        synchronized (this) {
+         while (queue.size() >= size) {
+             try {
+                 wait();
+             } catch (InterruptedException e) {
+                 Thread.currentThread().interrupt();
+                 e.printStackTrace();
+             }
+         }
+            this.queue.offer(value);
+            notifyAll();
+            System.out.println("Заполняем");
+        }
+    }
+
+    public T poll() {
+        T result = null;
+        synchronized (this) {
+            while (queue.isEmpty()) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    e.printStackTrace();
+                }
+            }
+            result = this.queue.poll();
+            System.out.println("Получаем - " + result);
+            notifyAll();
+        }
+        return result;
+    }
+}
+
---
+++ src/test/java/ru/job4j/concurrent/SimpleBlockingQueueTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.concurrent;
+
+/**
+ * @author Shegai Evgenii
+ * @version 1.0
+ * @since 27.05.2021
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SimpleBlockingQueueTest {
+
+    @Test
+    public void whenCreateProducerAndConsumer() throws InterruptedException {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(3);
+        Thread producer = new Thread(() -> {
+            System.out.println("Заполняем очередь");
+            queue.offer(1);
+            queue.offer(2);
+            queue.offer(3);
+        });
+
+        Thread consumer = new Thread(() -> {
+            System.out.println("Получаем из очереди");
+            queue.poll();
+            queue.poll();
+            queue.poll();
+        });
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        assertTrue(true);
+    }
+
+}
